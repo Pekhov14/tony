5ebc2332e8235409bcce10cef38fa904863c22ac
pekhov <pehovanton21@gmail.com>
2026-06-16 01:20:55 +0300

    full copy in rust

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufReader, Read};
-use serde_json::Value;
+use std::io::{BufReader};
+use std::process::Command;
+use colored::Colorize;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let tonyfile = File::open("tonyfile.json")?;
-    let reader = BufReader::new(tonyfile);
+    let args = std::env::args().collect::<Vec<String>>();
 
-    let map: HashMap<String, Value> = serde_json::from_reader(reader)?;
+    if args.len() < 2 {
+        // TODO: create help output and version
+        eprintln!("Usage: {} <command>", args[0]);
+        std::process::exit(1);
+    }
 
-    println!("{:#?}", map);
+    let command = &args[1];
+
+    let file = File::open("tonyfile.json")?;
+    let reader = BufReader::new(file);
+
+    let commands: HashMap<String, String> = serde_json::from_reader(reader)?;
+
+    if !commands.contains_key(command) {
+        eprintln!("Command not found: '{}'", command);
+        std::process::exit(1);
+    }
+
+    println!("{} {}", ">".green(), commands[command].blue().bold());
+    let (shell, flag) = if cfg!(target_os = "windows") {
+        ("cmd", "/C")
+    } else {
+        ("sh", "-c")
+    };
+
+    Command::new(shell)
+        .arg(flag)
+        .arg(commands[command].clone())
+        .status()?;
 
     Ok(())
 }
